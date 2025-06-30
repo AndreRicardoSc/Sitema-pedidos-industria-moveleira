2618fc99ce18b0c91d0e1b9d186fa901d110568d
Johann5731 <johannmborges27@gmail.com>
2025-06-30 12:58:38 -0300

    FEAT: Serializers Criados #3

---
+++ industria/serializers/__init__.py
@@ -0,0 +1,5 @@
+from .cliente import ClienteSerializer
+from .pedido import PedidoSerializer
+from .produto import ProdutoSerializer
+from .funcionario import FuncionarioSerializer
+from .materia import MateriaPrimaSerializer
